1ba04ec49022f95757bd7041b256a7da8b836179
alinanmorariu <alinan.morariu@yahoo.com>
2017-10-09 21:12:57 +0200

    refact

--- app/server.js
+++ app/server.js
@@ -2,13 +2,15 @@ var express = require("express");
 var app = express();
 
 app.get("/elements", function(req, res) {
-  res.send([{
+  var elements = [{
       AtomicNumber: "1", 
       Symbol: "H", 
       Name: "Hydrogen",
       AtomicWeight: "1.008",
       Group: "1"
-    }]);
+    }];
+
+  res.send(200, elements);
 });
 
 app.listen(3000);
